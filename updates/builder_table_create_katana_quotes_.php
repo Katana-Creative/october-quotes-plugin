514dcfb5cced9e76a0d03303a4617ac25a41f839
Jack Toothill <jt@katanalondon.com>
2018-03-16 17:45:24 +0000

    Prepared files for Github

---
+++ updates/builder_table_create_katana_quotes_.php
@@ -0,0 +1,23 @@
+<?php namespace Katana\Quotes\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateKatanaQuotes extends Migration
+{
+    public function up()
+    {
+        Schema::create('katana_quotes_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('quote');
+            $table->text('author');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('katana_quotes_');
+    }
+}
